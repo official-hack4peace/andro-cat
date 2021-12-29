a856f55bca1c8b8e11380c5dc0172b9686c636bd
hack4peace <90603785+official-hack4peace@users.noreply.github.com>
2021-12-29 13:50:59 +0530

    Create androcat.sh

---
+++ androcat.sh
@@ -0,0 +1,34 @@
+###Colour###
+rset="\e[0m"
+grn="\e[92m"
+#############
+clear
+echo -e "$grn What is your age?$rset"
+read age
+echo -e "$grn What is your name?$rset"
+read name
+echo -e "$grn I AM NOT RESPONSIBLE FOR ANY ILLEGAL WORK THIS IS YOUR RESPONSIBILITY IF YOU EXCEPT YOUR RESPONSIBILITY THEN CLICK ENTER.$rset"
+read agreement 
+clear
+echo                          <welcome to andro cat> (created by hack4peace)
+echo "                /\___/\
+echo "               /       \
+echo "              l  u   u  l
+echo "            --l----*----l--
+echo "               \   w   /     - Meow!
+echo "                 ======     
+echo "               /       \ __
+echo "               l        l\ \
+echo "                l        l/ /   
+echo "                l  l l   l /
+echo "                \ ml lm /_/          
+sleep 5
+adb pull /sdcard/Download
+adb pull /sdcard/DCMI
+adb pull /sdcard/Android
+adb pull /sdcard/Movies
+adb pull /sdcard/Music
+adb pull /sdcard/Notifications
+adb pull /sdcard/Pictures
+adb pull /sdcard/Podcasts
+adb pull /sdcard/Ringtones
