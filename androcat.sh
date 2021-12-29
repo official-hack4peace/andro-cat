2abc0376548a8d30ccaf2549adffc7fb41228130
hack4peace <90603785+official-hack4peace@users.noreply.github.com>
2021-12-29 14:17:55 +0530

    Update androcat.sh

--- androcat.sh
+++ androcat.sh
@@ -10,18 +10,18 @@ read name
 echo -e "$grn I AM NOT RESPONSIBLE FOR ANY ILLEGAL WORK THIS IS YOUR RESPONSIBILITY IF YOU EXCEPT YOUR RESPONSIBILITY THEN CLICK ENTER.$rset"
 read agreement 
 clear
-echo "                        <welcome to andro cat> (created by hack4peace)"
-echo "                /\___/\"
-echo "               /       \"
-echo "              l  u   u  l"
-echo "            --l----*----l--"
-echo "               \   w   /     - Meow!"
-echo "                 ====== "  
-echo "               /       \ __"
-echo "               l        l\ \"
-echo "               l        l/ /"
-echo "               l  l l   l /"
-echo "               \ ml lm /_/"        
+echo -e "$grn              <welcome to andro cat> (created by hack4peace)$rset"
+echo "                /\___/\ "
+echo "               /       \ "
+echo "              l  u   u  l "
+echo "            --l----*----l-- "
+echo "               \   w   /     - Meow! "
+echo "                ======= "  
+echo "               /       \ __ "
+echo "               l        l\ \ "
+echo "               l        l/ / "
+echo "               l  l l   l / "
+echo "               \ ml lm /_/ "        
 sleep 5
 adb pull /sdcard/Download
 adb pull /sdcard/DCMI
